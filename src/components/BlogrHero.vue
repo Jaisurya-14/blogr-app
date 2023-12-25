18d47904b74c9fbc0224cd6538e535acef483019
Jaisurya Narayanan <jaisurya@Jaisuryas-MacBook-Pro.local>
2023-12-25 11:18:45 +0530

    added hero section for app

---
+++ src/components/BlogrHero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="blogr-hero">
+    <div class="blogr-hero__con">
+      <h1 class="blogr-hero__title">A modern publishing platform</h1>
+      <p class="h5 m--b--2 blogr-hero__desc">
+        Grow your audience and build you online brand
+      </p>
+      <div class="blogr-hero__actions">
+        <button class="btn btn--primary m--r--1">Start for Free</button>
+        <button class="btn btn--secondary">Learn More</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "BlogrHero",
+};
+</script>
