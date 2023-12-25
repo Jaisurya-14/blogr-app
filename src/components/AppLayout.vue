f6b15d8184b1f24e45ba8011057144c92d12acfd
Jaisurya Narayanan <jaisurya@Jaisuryas-MacBook-Pro.local>
2023-12-25 11:16:19 +0530

    added APP layout component

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <MainHeader class="ps--a"></MainHeader>
+    </header>
+    <main>
+      <BlogrHero></BlogrHero>
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+import MainHeader from "./MainHeader.vue";
+import BlogrHero from "./BlogrHero.vue";
+export default {
+  name: "AppLayout",
+  components: {
+    MainHeader,
+    BlogrHero,
+  },
+};
+</script>
