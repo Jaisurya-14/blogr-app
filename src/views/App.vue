f4f9214a373fe9b7a3fd31fc3263430a44eeab38
Jaisurya Narayanan <jaisurya@Jaisuryas-MacBook-Pro.local>
2023-12-25 11:15:02 +0530

    Created App instance and view

---
+++ src/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <AppLayout />
+</template>
+<script>
+import AppLayout from "../components/AppLayout.vue";
+export default {
+  name: "App",
+  components: {
+    AppLayout,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../styles/styles.scss";
+</style>
