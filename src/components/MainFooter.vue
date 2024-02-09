5dcfbf31d78775af0fe8daca82804b946b0ab974
Jaisurya Narayanan <surya@Jaisuryas-MacBook-Air.local>
2024-02-09 19:28:28 +0530

    added main footer

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="footer container">
+    <a href="/" class="footer__logo">
+      <img src="../assets/images/logo.svg" alt="logo" />
+    </a>
+    <ul class="footer__nav">
+      <li class="footer__nav-item">
+        <h5 class="m--b--2 fw--bold">Product</h5>
+        <ul class="footer__nav__sub-list">
+          <li class="h6 m--b--1">
+            <a href="#">Overview</a>
+          </li>
+          <li class="h6 m--b--1">
+            <a href="#">Pricing</a>
+          </li>
+          <li class="h6 m--b--1">
+            <a href="#">Marketplace</a>
+          </li>
+          <li class="h6 m--b--1">
+            <a href="#">Features</a>
+          </li>
+          <li class="h6">
+            <a href="#">Integrations</a>
+          </li>
+        </ul>
+      </li>
+      <li class="footer__nav-item">
+        <h5 class="m--b--2 fw--bold">Company</h5>
+        <ul class="footer__nav__sub-list">
+          <li class="h6 m--b--1">
+            <a href="#">About</a>
+          </li>
+          <li class="h6 m--b--1">
+            <a href="#">Team</a>
+          </li>
+          <li class="h6 m--b--1">
+            <a href="#">Blog</a>
+          </li>
+          <li class="h6">
+            <a href="#">Careers</a>
+          </li>
+        </ul>
+      </li>
+      <li class="footer__nav-item">
+        <h5 class="m--b--2 fw--bold">Connect</h5>
+        <ul class="footer__nav__sub-list">
+          <li class="h6 m--b--1">
+            <a href="#">Contact</a>
+          </li>
+          <li class="h6 m--b--1">
+            <a href="#">Newsletter</a>
+          </li>
+          <li class="h6">
+            <a href="#">LinkedIn</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainFooter",
+};
+</script>
